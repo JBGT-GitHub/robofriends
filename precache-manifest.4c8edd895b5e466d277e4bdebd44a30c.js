b774f1c28d8cfea0e153a7f6b3d22215603d5634
JBGT-GitHub <47192342+JBGT-GitHub@users.noreply.github.com>
2019-01-30 21:19:31 -0500

    Updates

---
+++ precache-manifest.4c8edd895b5e466d277e4bdebd44a30c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "b78228056054494fb09b",
+    "url": "/robofriends/static/js/main.b7822805.chunk.js"
+  },
+  {
+    "revision": "2d65b7f7fce6941c93ae",
+    "url": "/robofriends/static/js/1.2d65b7f7.chunk.js"
+  },
+  {
+    "revision": "b78228056054494fb09b",
+    "url": "/robofriends/static/css/main.65b98ddd.chunk.css"
+  },
+  {
+    "revision": "2d65b7f7fce6941c93ae",
+    "url": "/robofriends/static/css/1.a3a5f5cb.chunk.css"
+  },
+  {
+    "revision": "4c9e368cfb7251d0b3135cc1dce4356b",
+    "url": "/robofriends/index.html"
+  }
+];
